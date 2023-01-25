2610fdeea684e518d2321efbcb6ba439ef9f6ff8
Tyler <tylerlvvu@gmail.com>
2023-01-24 22:56:21 -0800

    basis

---
+++ index.js
@@ -0,0 +1,22 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const inquirer = require ("inquirer")
+const fs = require ("fs")
+const questions = [
+    {
+        
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+
+
